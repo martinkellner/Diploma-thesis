e1b6a2e0de2243936a07c622ed14a44619b2f0a1
Martin <martinkellner@github.com>
2019-05-23 17:24:15 +0200

    asd

--- code/generator/kill.sh
+++ code/generator/kill.sh
@@ -1,3 +1,3 @@
-killall yarpserver yarprobotinterface iKinGazeCtrl iKinCartesianSolver
+killall yarpserver yarpview yarprobotinterface iKinGazeCtrl iKinCartesianSolver
 ps au | grep -E "^*+iCub_SIM$" | grep -oE '[0-9]*' | head -n 1 | xargs kill -9
 
