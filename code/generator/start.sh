2e273d7d107a1fec573353c095718debdb36e221
Martin <martinkellner@github.com>
2019-05-23 17:13:27 +0200

    start update

--- code/generator/start.sh
+++ code/generator/start.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 yarpserver &
-
 sleep 2
-
 iCub_SIM & 
-
 sleep 2 
-
 yarprobotinterface --context simCartesianControl &
- 
 sleep 2
-
 iKinCartesianSolver --part right_arm &
-
 sleep 2
-
 iKinGazeCtrl --robot icubSim --imu::mode off &
+sleep 2
+yarpview --name /cameraleft &
+sleep 2
+yarp connect /icubSim/cam/left /cameraleft &
+sleep 2
+yarpview --name /cameraright &
+sleep 2
+yarp connect /icubSim/cam/right /cameraright &
+
